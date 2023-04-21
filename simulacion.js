d1de4b445d2130278d99ffb31a7ae5774d7b70fb
LuisS74 <89611142+LuisS74@users.noreply.github.com>
2023-04-21 15:49:06 -0400

    subida final

--- simulacion.js
+++ simulacion.js
@@ -1,8 +1,45 @@
-const personaje = {
-    nombre: "hola",
-    vidaMax: 100,
-    vidaAct: 100,
-    Daño: 12
+let combate = function () {
+    const Heroe = {
+        nombre: "Heroe",
+        vidaMax: 100,
+        vidaAct: 100,
+        daño: Math.round(Math.random() * 10)
+    }
+
+    const Limo = {
+        nombre: "Limo",
+        vidaMax: 100,
+        vidaAct: 100,
+        daño: Math.round(Math.random() * 10)
+    }
+
+    while (Heroe.vidaAct > 0) {
+        ataca(Heroe, Limo)
+        mostrarStats(Limo, Heroe)
+        if (Limo.vidaAct <= 0) {
+            break;
+        }
+        ataca(Heroe, Limo);
+        mostrarStats(Heroe, Limo)
+    }
+    console.log(`¡Combate concluido!`)
 }
-   
-console.log(personaje.vidaAct)
+
+let ataca = function (ataque, recibe) {
+    recibe.vidaAct = recibe.vidaAct - ataque.daño
+    console.log(`¡${ataque.nombre} hace ${ataque.daño} puntos de daño a ${recibe.nombre}!`)
+}
+
+let mostrarStats = function (personaje1, personaje2) {
+    console.log(`${personaje1.nombre} - ${personaje1.vidaAct}/${personaje1.vidaMax}`)
+    console.log(`${personaje2.nombre} - ${personaje2.vidaAct}/${personaje2.vidaMax}`)
+}
+
+combate()
+
+
+
+
+
+
+
